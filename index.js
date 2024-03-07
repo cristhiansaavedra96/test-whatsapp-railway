8031ab198d36dbd65feae2d4c7da76c37d9b6c43
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2024-03-07 11:20:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ const client = new Client({
 	},
 });
 
+client.on('requestfailed', request => {
+	console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`)
+});
+// Catch console log errors
+client.on("pageerror", err => {
+	console.log(`Page error: ${err.toString()}`);
+});
+
+client.on('authenticated', (session) => {
+	console.log('AUTHENTICATED', session);
+});
+
+client.on('auth_failure', msg => {
+	console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('disconnected', (reason) => {
+	console.log('Client was logged out', reason);
+});
+
 client.on('qr', (qr) => {
 	//qr code smallest posible
 	qrcode.generate(qr, { small: true });
@@ -33,6 +53,17 @@ client.on('message', async (message) => {
 	if (message.body === '!ping') {
 		await client.sendMessage(message.from, 'pong');
 	}
+});
+
+//unhandledRejection
+process.on('unhandledRejection', (reason, promise) => {
+	console.log('Unhandled Rejection at:', reason.stack || reason);
+});
+
+//uncaughtException
+
+process.on('uncaughtException', (error) => {
+	console.log('Uncaught Exception thrown', error.stack || error);
 });
 
 client.initialize();
